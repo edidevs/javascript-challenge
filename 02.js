c083fdfadeb28c78fb1cdbc179d831cd87da6a0e
thejuniordevops <edigithub@gmail.com>
2018-07-18 04:44:34 -0400

    second task

---
+++ 02.js
@@ -0,0 +1,17 @@
+/*
+#2 Combine several arrays
+02.s
+Combine all arrays into one sentence 
+
+*/
+
+const first = ['Behind', 'every', 'great', 'man']; 
+const second = ['is', 'a', 'woman']; 
+const third = ['rolling', 'her', 'eyes']; 
+
+//combine all arrays into one readable sentence
+let arrayAll = first.concat(second, third).join(" ");  //Behind every great man is a woman rolling her eyes
+
+
+
+ 
