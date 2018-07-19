5072e9e7d4272be4d2840feec4450dbe6418ee8f
Edi <40353224+thejuniordevops@users.noreply.github.com>
2018-07-19 19:19:11 -0400

    Create import.js

---
+++ import.js
@@ -0,0 +1,14 @@
+ /*
+import.js
+
+
+
+*/
+
+var output = require('./module');
+
+console.log(output.upper("hello")); 
+console.log(output.count("hello"));
+console.log(output.countWords("hello")); 
+console.log(output.trim("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")); 
+console.log(output.trimWords("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")); 
